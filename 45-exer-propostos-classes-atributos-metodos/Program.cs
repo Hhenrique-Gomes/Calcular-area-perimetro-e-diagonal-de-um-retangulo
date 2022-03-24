d89092ee5abd6a9dcab553f5fb0ea81bddd505f0
Hhenrique-Gomes <henriquegomes99h@gmail.com>
2022-03-24 12:13:56 -0300

    Software para calcular area, perimetro e diagonal de um retangulo

---
+++ 45-exer-propostos-classes-atributos-metodos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _45_exer_propostos_classes_atributos_metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            calculos j = new calculos();
+            
+            Console.WriteLine("Digite a altura do retangulo ");
+            j.Altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a largura do retangulo");
+            j.Largura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Altura do retangulo: {0}", j.Altura);
+            Console.WriteLine("Largura do retangulo: {0}", j.Largura);
+
+            Console.WriteLine("Area: {0}",j.Area());
+            Console.WriteLine("Perimetro: {0}",j.Perimetro());
+            Console.WriteLine("Diagonal: {0}", j.Diagonal());
+
+
+
+        }
+    }
+}
---
+++ 45-exer-propostos-classes-atributos-metodos/calculos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _45_exer_propostos_classes_atributos_metodos
+{
+    class calculos
+    {
+
+        public double Altura;
+        public double Largura;
+
+        public double Area()
+        {
+
+            return Altura * Largura;
+
+        } 
+
+        public double Perimetro()
+        {
+
+            return 2 * (Altura + Largura);
+
+        }
+
+        public double Diagonal()
+        {
+
+            return Math.Sqrt(Altura * Altura + Largura * Largura);
+
+        }
+
+    }
+}
